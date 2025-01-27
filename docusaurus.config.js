e48017f5e30979979a5ca61d0f4c922acb11081f
rweesner <rweesner@smatechnologies.com>
2025-01-27 06:54:43 -0600

    First Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,50 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'SMA Technologies Help',
+  tagline: 'WebServices ACS',
+  url: 'https://help.smatechnologies.com',
+  baseUrl: '/opcon/conenctors/webservices-acs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'smatechnologies',
+  projectName: 'webservices-acs-docs',
+  themeConfig: {
+    navbar: {
+      title: 'Help',
+      logo: {
+        alt: 'SMA Technologies Help Logo',
+        src: 'img/logo.svg',
+        href: 'https://help.smatechnologies.com',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} SMA Technologies.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          breadcrumbs: false,
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/smatechnologies/webservices-acs/blob/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+//  plugins: [
+//    [
+//      require.resolve('@cmfcmf/docusaurus-search-local'), 
+//      {
+//      }
+//    ],
+//  ],
+};
